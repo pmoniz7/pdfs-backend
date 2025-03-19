3b354f2be6edf5a9b038b1aaf62b6ab376ef47d6
unknown <pmoniz7@hotmail.com>
2025-03-18 22:22:37 -0300

    alteração database.py url única

--- config.py
+++ config.py
@@ -3,12 +3,12 @@ import boto3
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_HOST: str
-    DATABASE_NAME: str
-    DATABASE_USER: str
-    DATABASE_PASSWORD: str
-    DATABASE_PORT: int
-    app_name: str = "Full Stack PDF CRUD App"
+    ###DATABASE_HOST: str
+    ###DATABASE_NAME: str
+    ###DATABASE_USER: str
+    ###DATABASE_PASSWORD: str
+    ###DATABASE_PORT: int
+    ###app_name: str = "Full Stack PDF CRUD App"
     AWS_KEY: str
     AWS_SECRET: str
     #AWS_S3_BUCKET: str = "pdf-basic-app"  #Paulo Roberto Moniz - comentou essa linha #
