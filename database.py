ab59fe5c5574efb3363fc2ecd16c51e9b18d4c1d
unknown <pmoniz7@hotmail.com>
2025-03-18 21:57:19 -0300

    alteração database.py url única

--- database.py
+++ database.py
@@ -8,14 +8,16 @@ load_dotenv()
 
 #SQLALCHEMY_DATABASE_URL = f"postgresql://{os.environ['DATABASE_USER']}:@{os.environ['DATABASE_HOST']}/{os.environ['DATABASE_NAME']}"
 
-user = os.environ['DATABASE_USER']
-password = os.environ['DATABASE_PASSWORD']
-host = os.environ['DATABASE_HOST']
-port = os.environ['DATABASE_PORT']
-db_name = os.environ['DATABASE_NAME']
+###user = os.environ['DATABASE_USER']
+###password = os.environ['DATABASE_PASSWORD']
+###host = os.environ['DATABASE_HOST']
+###port = os.environ['DATABASE_PORT']
+###db_name = os.environ['DATABASE_NAME']
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
+###SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
+SQLALCHEMY_DATABASE_URL = f"postgresql://basepdfs_4srh_user:FTabMoWIG6cSiSf99m1jt30jp3pZLcYL@dpg-cvcnv0l2ng1s7390lpc0-a/basepdfs_4srh"
 
+                                         
 print("%%%%%%%% SQLALCHEMY_DATABASE_URL %%%%%%%%% ==========> ", SQLALCHEMY_DATABASE_URL)
 
 engine = create_engine(
